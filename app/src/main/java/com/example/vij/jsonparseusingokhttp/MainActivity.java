fade96f796df8963e6de98fc8fc96067bec9c38f
Vijay Bheda <bhedavijay489@gmail.com>
2016-03-13 12:24:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vij/jsonparseusingokhttp/MainActivity.java
@@ -0,0 +1,222 @@
+package com.example.vij.jsonparseusingokhttp;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.vij.jsonparseusingokhttp.adapter.MyArrayAdapter;
+import com.example.vij.jsonparseusingokhttp.model.MyDataModel;
+import com.example.vij.jsonparseusingokhttp.parser.JSONParser;
+import com.example.vij.jsonparseusingokhttp.utils.InternetConnection;
+import com.example.vij.jsonparseusingokhttp.utils.Keys;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<MyDataModel> list;
+    private MyArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /**
+         * Array List for Binding Data from JSON to this List
+         */
+        list = new ArrayList<>();
+        /**
+         * Binding that List to Adapter
+         */
+        adapter = new MyArrayAdapter(this, list);
+
+        /**
+         * Getting List and Setting List Adapter
+         */
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(findViewById(R.id.parentLayout), list.get(position).getName() + " => " + list.get(position).getPhone(), Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        /**
+         * Just to know onClick and Printing Hello Toast in Center.
+         */
+        Toast toast = Toast.makeText(getApplicationContext(), "Click on FloatingActionButton to Load JSON", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View view) {
+
+                /**
+                 * Checking Internet Connection
+                 */
+                if (InternetConnection.checkConnection(getApplicationContext())) {
+                    new GetDataTask().execute();
+                } else {
+                    Snackbar.make(view, "Internet Connection Not Available", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Creating Get Data Task for Getting Data From Web
+     */
+    class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setTitle("Hey Wait Please...");
+            dialog.setMessage("I am getting your JSON");
+            dialog.show();
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            /**
+             * Getting JSON Object from Web Using okHttp
+             */
+            JSONObject jsonObject = JSONParser.getDataFromWeb();
+
+            try {
+                /**
+                 * Check Whether Its NULL???
+                 */
+                if (jsonObject != null) {
+                    /**
+                     * Check Length...
+                     */
+                    if(jsonObject.length() > 0) {
+                        /**
+                         * Getting Array named "contacts" From MAIN Json Object
+                         */
+                        JSONArray array = jsonObject.getJSONArray(Keys.KEY_CONTACTS);
+
+                        /**
+                         * Check Length of Array...
+                         */
+                        int lenArray = array.length();
+                        if(lenArray > 0) {
+                            for(int jIndex = 0; jIndex < lenArray; jIndex++) {
+
+                                /**
+                                 * Creating Every time New Object
+                                 * and
+                                 * Adding into List
+                                 */
+                                MyDataModel model = new MyDataModel();
+
+                                /**
+                                 * Getting Inner Object from contacts array...
+                                 * and
+                                 * From that We will get Name of that Contact
+                                 *
+                                 */
+                                JSONObject innerObject = array.getJSONObject(jIndex);
+                                String name = innerObject.getString(Keys.KEY_NAME);
+                                String email = innerObject.getString(Keys.KEY_EMAIL);
+                                String image = innerObject.getString(Keys.KEY_PROFILE_PIC);
+
+                                /**
+                                 * Getting Object from Object "phone"
+                                 */
+                                JSONObject phoneObject = innerObject.getJSONObject(Keys.KEY_PHONE);
+                                String phone = phoneObject.getString(Keys.KEY_MOBILE);
+
+                                model.setName(name);
+                                model.setEmail(email);
+                                model.setPhone(phone);
+                                model.setImage(image);
+
+                                /**
+                                 * Adding name and phone concatenation in List...
+                                 */
+                                list.add(model);
+                            }
+                        }
+                    }
+                } else {
+
+                }
+            } catch (JSONException je) {
+                Log.i(JSONParser.TAG, "" + je.getLocalizedMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dialog.dismiss();
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if(list.size() > 0) {
+                adapter.notifyDataSetChanged();
+            } else {
+                Snackbar.make(findViewById(R.id.parentLayout), "No Data Found", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
